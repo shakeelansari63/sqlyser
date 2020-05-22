50bf85b80a90ab24a6de33174e0ae8c5735eebae
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-05-22 21:37:05 +0530

    Get Source and Target Table Logic added

--- sqlyser/sql_new.py
+++ sqlyser/sql_new.py
@@ -5,6 +5,19 @@ from collections import OrderedDict
 
 class SQL:
     """SQl Class for SQL objects"""
+    # Constant
+    TBL_PATTERN = r'([a-z0-9\$\#\"\&\.\-\_]*) '
+    LIT_TEMPLATE = '&&-LITERAL{idx}-&&'
+    LIT_TEMPLATE = '&&-LITERAL{idx}-&&'
+    FNC_TEMPLATE = '&&-FUNCTION{idx}-&&'
+    COL_TEMPLATE = '&&-COLUMNLIST{idx}-&&'
+    GRP_TEMPLATE = '&&-GROUPLIST{idx}-&&'
+    ORD_TEMPLATE = '&&-ORDERLIST{idx}-&&'
+    HAV_TEMPLATE = '&&-HAVINGLIST{idx}-&&'
+    FORMAT_NEWLINE_KEYWORD = r"^SELECT |^SEL | SELECT | SEL | FROM | INNER\s+JOIN " + \
+        r"| LEFT[OUTER\s]+JOIN | RIGHT[OUTER\s]+JOIN | FULL[OUTER\s]+JOIN " + \
+        r"| CROSS\s+JOIN | JOIN | GROUP BY | ORDER BY | ON | WHERE " + \
+        r"| AND | HAVING | \( | \) | , "
 
     def __init__(self, raw_sql=''):
         self.sql = raw_sql
@@ -30,10 +43,13 @@ class SQL:
     def __str__(self):
 
         # Return Decoded re-formated SQL
-        return f"Clean SQL: {self.clean_sql}\n\nClean Encoded SQL: {self._clean_encoded_sql}\n\nEncoder Dictionary: {self.__get_encoder_dict()}"
+        return "Clean SQL: {cleansql}\n\n".format(cleansql=self.clean_sql) + \
+            "Clean Encoded SQL: {enc_sql}\n\n".format(enc_sql=self._clean_encoded_sql) + \
+            "Encoder Dict: {enc_dict}".format(
+                enc_dict=self.__get_encoder_dict())
 
     def __get_encoder_dict(self):
-        return "\n".join([f"{key} : {value}" for key, value in self._encoder_dict.items()])
+        return "\n".join(["{key} : {value}".format(key=key, value=value) for key, value in self._encoder_dict.items()])
 
     def __decode_sql(self, input_sql):
         # Initialize Decoded SQL with clean SQL
@@ -55,7 +71,7 @@ class SQL:
             for index, func in enumerate(functions_list, start=1):
                 if self.__is_valid_function(func):
                     # Generate Key for function encoding
-                    key = '&&-FUNCTION' + str(index) + '-&&'
+                    key = self.FNC_TEMPLATE.format(idx=str(index))
                     self._encoder_dict[key] = func
 
                     # Encode the Function
@@ -69,7 +85,7 @@ class SQL:
 
         if len(select_from_list) > 0:
             for index, col_list_str in enumerate(select_from_list, start=1):
-                key = '&&-COLUMNLIST' + str(index) + '-&&'
+                key = self.COL_TEMPLATE.format(idx=str(index))
 
                 # Add to dictionary
                 self._encoder_dict[key] = col_list_str
@@ -106,7 +122,7 @@ class SQL:
 
             for index, sub_str in enumerate(single_quoted_substr, start=1):
                 # Generate keys
-                key = '&&-LITERAL' + str(index) + '-&&'
+                key = self.LIT_TEMPLATE.format(idx=str(index))
                 self._encoder_dict[key] = sub_str
 
                 # Enocode substrings in SQL
@@ -155,36 +171,95 @@ class SQL:
         # Encode the Functions
         self.__encode_column_list()
 
-    def __reformat_encoded_sql(self):
+    def reformat(self, sql):
         # Initialize Reformatted SQL
-        reformated_clean_encoded_sql = self._clean_encoded_sql
+        reformated_sql = sql
 
-        new_line_keywords = re.compile(r"^SELECT |^SEL | SELECT | SEL | FROM | JOIN | BY | \( | \)")\
-            .findall(self._clean_encoded_sql)
+        new_line_keywords = re.compile(
+            self.FORMAT_NEWLINE_KEYWORD).findall(sql)
 
         for sql_keyword in set(new_line_keywords):
-            reformatted_sql = reformated_clean_encoded_sql.replace(
-                sql_keyword, sql_keyword + '\n')
-            reformated_clean_encoded_sql = reformatted_sql
+            reformatted_sql = reformated_sql.replace(
+                sql_keyword, '\n' + sql_keyword)
+            reformated_sql = reformatted_sql
 
-        return reformated_clean_encoded_sql
+        return reformated_sql
 
     @property
     def clean_sql(self):
-        clean_sql = self.__decode_sql(self.__reformat_encoded_sql())
+        clean_sql = self.__decode_sql(
+            self.reformat(self._clean_encoded_sql))
         return clean_sql
 
     @property
     def target_table(self):
-        target_table = None
-        # ToDo Generate target Table Name
-        return target_table
+        target_table = []
+
+        sql_type = self.sql_type
+
+        # Begining of valid SQL
+        if sql_type in ('CREATE TABLE', 'REPLACE TABLE'):
+            table_name_re = [re.compile(r'^CREATE .*?TABLE ' + self.TBL_PATTERN, re.I),
+                             re.compile(r'^REPLACE .*?TABLE ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type == 'INSERT':
+            table_name_re = [re.compile(r'INSERT INTO ' + self.TBL_PATTERN, re.I),
+                             re.compile(r'INS INTO ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type == 'UPDATE':
+            table_name_re = [re.compile(r'^UPDATE ' + self.TBL_PATTERN, re.I),
+                             re.compile(r'^UPD ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type == 'DELETE':
+            table_name_re = [re.compile(r'^DELETE ' + self.TBL_PATTERN, re.I),
+                             re.compile(r'^DEL ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type == 'MERGE':
+            table_name_re = [re.compile(
+                r'^MERGE INTO ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type == 'EXECUTE MACRO':
+            table_name_re = [re.compile(r'^EXECUTE ' + self.TBL_PATTERN, re.I),
+                             re.compile(r'^EXEC ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type == 'CALL PROCEDURE':
+            table_name_re = [re.compile(r'^CALL ' + self.TBL_PATTERN, re.I)]
+
+        elif sql_type != 'SELECT':
+            table_name_re = [re.compile(
+                r'^' + sql_type + ' ' + self.TBL_PATTERN, re.I)]
+
+        else:
+            return None
+
+        for tbl_regexp in table_name_re:
+            target_table.extend(re.findall(
+                tbl_regexp, self._clean_encoded_sql))
+
+        if target_table:
+            return target_table[0].strip()
+        else:
+            return None
 
     @property
     def source_tables(self):
-        source_tables = set([])
-        # ToDo Generate source Tables List
-        return source_tables
+        source_tables = []
+
+        # Regular expressions for Source Tables
+        table_name_re = [
+            re.compile(r' FROM ' + self.TBL_PATTERN, re.I),
+            re.compile(r' JOIN ' + self.TBL_PATTERN, re.I),
+            re.compile(r' , ' + self.TBL_PATTERN, re.I),
+            re.compile(r' USING ' + self.TBL_PATTERN, re.I)
+        ]
+
+        # Find tables matching the regular expression
+        for tbl_regexp in table_name_re:
+            source_tables.extend(re.findall(
+                tbl_regexp, self._clean_encoded_sql))
+
+        # Return List of unique tables
+        return list(set(source_tables))
 
     @property
     def sql_type(self):
@@ -193,54 +268,42 @@ class SQL:
             else self._clean_encoded_sql.split(' ')
 
         # Translate Acronyms in Teradata
-        if sql_type[0] in ('INS', 'INSERT'):
+        if sql_type[0] == 'INS':
             return 'INSERT'
-        elif sql_type[0] in ('SEL', 'SELECT', 'WITH'):
+
+        elif sql_type[0] in ('SEL', 'WITH'):
             return 'SELECT'
-        elif sql_type[0] in ('DEL', 'DELETE'):
+
+        elif sql_type[0] == 'DEL':
             return 'DELETE'
-        elif sql_type[0] in ('UPD', 'UPDATE'):
+
+        elif sql_type[0] == 'UPD':
             return 'UPDATE'
-        elif sql_type[0] in ('CREATE', 'REPLACE') and 'TABLE' in sql_type:
-            return 'CREATE TABLE'
-        elif sql_type[0] in ('CREATE', 'REPLACE') and 'VIEW' in sql_type:
-            return 'CREATE VIEW'
-        elif sql_type[0] in ('CREATE', 'REPLACE') and 'MACRO' in sql_type:
-            return 'CREATE MACRO'
-        elif sql_type[0] in ('CREATE', 'REPLACE') and 'PROCEDURE' in sql_type:
-            return 'CREATE PROCEDURE'
-        elif sql_type[0] in ('CREATE', 'REPLACE') and 'FUNCTION' in sql_type:
-            return 'CREATE FUNCTION'
-        elif sql_type[0] == 'DROP' and 'TABLE' in sql_type:
-            return 'DROP TABLE'
-        elif sql_type[0] == 'DROP' and 'VIEW' in sql_type:
-            return 'DROP VIEW'
-        elif sql_type[0] == 'DROP' and 'MACRO' in sql_type:
-            return 'DROP MACRO'
-        elif sql_type[0] == 'DROP' and 'PROCEDURE' in sql_type:
-            return 'DROP PROCEDURE'
-        elif sql_type[0] == 'DROP' and 'FUNCTION' in sql_type:
-            return 'DROP FUNCTION'
-        elif sql_type[0] == 'ALTER' and 'TABLE' in sql_type:
-            return 'ALTER TABLE'
+
         elif sql_type[0] in ('EXEC', 'EXECUTE'):
             return 'EXECUTE MACRO'
+
         elif sql_type[0] == 'CALL':
             return 'CALL PROCEDURE'
-        elif sql_type[0] in ('GRANT', 'REVOKE', 'MERGE'):
-            return sql_type[0]
+
+        elif sql_type[0] in ('CREATE', 'REPLACE') and 'TABLE' in sql_type:
+            return '{sql_tp} TABLE'.format(sql_tp=sql_type[0])
+
+        elif sql_type[0] in ('CREATE', 'REPLACE', 'DROP', 'ALTER'):
+            return '{sql_tp1} {sql_tp2}'.format(sql_tp1=sql_type[0], sql_tp2=sql_type[1])
+
         else:
-            return None
+            return sql_type[0]
 
     @property
     def sql_lang(self):
-        if self.sql_type in ('SELECT', 'INSERT', 'UPDATE', 'DELETE', 'MERGE'):
+        sql_type = self.sql_type.split(' ')[0]
+
+        if sql_type in ('SELECT', 'INSERT', 'UPDATE', 'DELETE', 'MERGE'):
             return 'DML'
-        elif self.sql_type in ('CREATE TABLE', 'CREATE VIEW', 'CREATE MACRO', 'CREATE PROCEDURE',
-                               'CREATE FUNCTION', 'DROP TABLE', 'DROP VIEW', 'DROP MACRO',
-                               'DROP PROCEDURE', 'DROP FUNCTION', 'ALTER TABLE'):
+        elif sql_type in ('CREATE', 'DROP', 'REPLACE', 'ALTER'):
             return 'DDL'
-        elif self.sql_type in ('GRANT', 'REVOKE'):
+        elif sql_type in ('GRANT', 'REVOKE'):
             return 'DCL'
         return None
 
--- test/test_sqlyser.py
+++ test/test_sqlyser.py
@@ -15,7 +15,7 @@ substr(column_g, 1, 15) as column_g_sub, extract( day from current_date) as date
 , substr(' Function with literal', 1, 5) as column_h
 , case when ( some_col = x or some_other_col = y) and (yet_another_column=z)
 then 'do this' else 'do new this' end as case_col
-from db1.tbl1 tb1 -- Table 1
+from db1.tbl1 tb1, dbs.tbs -- Table 1 and comma seperated table name
 inner join db1. tbl2 tb2 /** Multi line SQL inside
 sql
 -- lets see how it goes
@@ -27,6 +27,7 @@ left outer join ( -- starting subquery
     on tb2.xcol = tb3.ycol
     where tb2.some_col = 5 /* Literal filter */
 ) tb3 on tb1 .jc2 = tb3 .jc2 -- I am funny guy and I can put comment anywhere ###############
+Right Join db4.tbl4 rtbl4 on rtbl4.col4 = tbl1 . col1 /* Right Join without Outer */
 order by column_a
 ; -- I can also nest the comments /** like this **/
 """
@@ -35,8 +36,17 @@ sql1 = SQL(sqlstr)
 print(sql1, '\n')
 print(sql1.sql_type)
 print(sql1.sql_lang)
+print(sql1.target_table)
+print(sql1.source_tables)
 
 sql2 = SQL()
 print(sql2)
-sql2.sql = "Select * from db.tbl"
+sql2.sql = """Select tbl.col1, tbl.col2, tbl.col3, tbl2.col4, substr(tbl2.col4, 2, 2),
+tbl2.col5, max(tbl.numval) from db.tbl, db.tbl2
+where tbl.col1 (+) = tbl2.col1 -- Oracle style Join
+and tbl.col2 = 5 -- Literal Join
+group by tbl.col1, tbl.col2, tbl.col3, tbl2.col4, substr(tbl2.col4, 2, 2), /* Groups */
+tbl2.col5
+having max(tbl.numval) > 20 /*** Having clause ***/
+"""
 print(sql2)
